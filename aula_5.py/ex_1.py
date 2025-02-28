557f498dea812a721c01e6f823f373d96d6e2dc3
1berto <144922529+1bertoW@users.noreply.github.com>
2025-02-28 14:25:55 -0300

    Add files via upload

---
+++ aula_5.py/ex_1.py
@@ -0,0 +1,8 @@
+contador = 0 
+
+
+while contador < 5:
+    print("entrei mas já vou sair")
+    contador = contador + 1
+
+print("vou ser executador jájá")
---
+++ aula_5.py/ex_3.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+while True:
+    print(20*'-')
+    print(f"{'calculadora':^20}")
+    print(20*'-')
+    print("[0] somar")
+    print("[1] subtrair")
+    print(20*'-')
+    opcao = input("digite uma opção ou 's' para sair:")
+
+    if opcao == 's' or opcao == 'S':
+        break
+print("até logo...")
+
